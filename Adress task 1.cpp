09b777b294443ccde9137b3c2bad2c7d7e31a98b
Klea Helga <elaginas27@gmail.com>
2025-05-24 16:31:43 +0300

    commit 1

---
+++ Adress task 1.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+class Adress {
+private:
+	string _city;
+	string _street;
+	int _house;
+	int _flat;
+
+	void outFile(Adress adress, ofstream& outtxt) {
+		outtxt << adress._city << ", " << adress._street << ", " << adress._house << ", " << adress._flat << endl;
+	}
+
+public:
+	Adress() {}
+	Adress(string city, string street, int house, int flat) :_city{ city }, _street{ street }, _house{ house }, _flat{ flat } {}
+	void inFile(int counter, ifstream& intxt, ofstream& outtxt) {
+		Adress* adress = new Adress[counter];
+		for (int i = 0; i < counter; i++) {
+			string city, street;
+			int house, flat;
+			intxt >> city >> street >> house >> flat;
+			adress[i] = Adress(city, street, house, flat);
+		}
+		for (int i = counter - 1; i >= 0; i--) {
+			adress[i].outFile(adress[i], outtxt);
+		}
+		delete[] adress;
+	}
+};
+
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	ifstream intxt("C:\\Users\\katen\\Documents\\in.txt");
+	ofstream outtxt("C:\\Users\\katen\\Documents\\out.txt");
+	if (!intxt.is_open()) {
+		cout << "Ошибка открытия файла" << endl;
+	}
+	int counter;
+	intxt >> counter;
+	outtxt << counter << endl;
+	Adress adresses;
+	adresses.inFile(counter, intxt, outtxt);
+	intxt.close();
+	outtxt.close();
+	return 0;
+
+}
